422baec714413a5a5544172c3a5865e18474790a
Galen Holt <53928785+galenholt@users.noreply.github.com>
2023-01-06 14:49:18 +1100

    giving up on onLoad until we have a py package

--- werptoolkitr/R/zzz.R
+++ werptoolkitr/R/zzz.R
@@ -6,7 +6,7 @@ controller_functions <- NULL
   reticulate::configure_environment(pkgname)
 
 
-  controller_functions <<- reticulate::import_from_path("controller_functions", path = system.file("python", package = 'werptoolkitr'), delay_load = TRUE)
+  # controller_functions <<- reticulate::import_from_path("controller_functions", path = system.file("python", package = 'werptoolkitr'), delay_load = TRUE)
 
   # also works, closer to the reticulate::import(module) we eventually want
   # controller_functions <<- reticulate::import_from_path("controller_functions", "../werptoolkitpy/werptoolkitpy/controller_functions", delay_load = TRUE)
