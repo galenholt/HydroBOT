3e8b4f7c257af0d4be11cbc0adbca66f60c39f12
Galen Holt <53928785+galenholt@users.noreply.github.com>
2023-10-26 14:10:47 +1100

    add scenariofilter to avoid reading all hydros

--- R/read_hydro.R
+++ R/read_hydro.R
@@ -3,14 +3,15 @@
 #' @param hydropath path to the hydrographs
 #' @param long logical, default TRUE. Do we want the data long (with a column for gauge number) or wide (FALSE, with gauge numbers as columns)
 #' @param format character, default 'csv'. Format of the hydrographs, determines which reading function gets called.
+#' @param scenariofilter character vector of scenario names to include. Default `NULL` includes all
 #'
 #' @return tibble of hydrographs
 #' @export
 #'
 #' @examples
-read_hydro <- function(hydropath, long = TRUE, format = 'csv') {
+read_hydro <- function(hydropath, scenariofilter = NULL, long = TRUE, format = 'csv') {
   if (format == 'csv') {
-    return(read_hydro_csv(hydropath, long))
+    return(read_hydro_csv(hydropath, scenariofilter, long))
   }
 }
 
@@ -30,8 +31,14 @@ read_hydro <- function(hydropath, long = TRUE, format = 'csv') {
 #' @export
 #'
 #' @examples
-read_hydro_csv <- function(hydropath, long) {
+read_hydro_csv <- function(hydropath, scenariofilter, long) {
   all_hydros <- list.files(hydropath, recursive = TRUE, pattern = "*.csv")
+
+  if (!is.null(scenariofilter)) {
+    all_hydros <- all_hydros[stringr::str_which(all_hydros,
+                                                pattern = stringr::str_flatten(scenariofilter, collapse = '|'))]
+  }
+
   # read-in and extract the names
   hydros <- readr::read_csv(file.path(hydropath, all_hydros),
                             id = 'scenario', show_col_types = FALSE) |>
