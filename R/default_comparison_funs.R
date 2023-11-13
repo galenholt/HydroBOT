379857f42ec6f889d8ea97cefd76a8015447de57
Galen Holt <53928785+galenholt@users.noreply.github.com>
2023-11-13 17:11:23 +1100

    adding odds ratio

--- R/default_comparison_funs.R
+++ R/default_comparison_funs.R
@@ -39,4 +39,34 @@ relative <- function(x,y, add_eps = 0) {
                    na.rm = TRUE)/2
   }
   (x + add_eps)/(y + add_eps)
+}
+
+
+#' Odds ratio
+#'
+#' @param x data to compare
+#' @param y reference data
+#' @param add_eps default 0. epsilon to add to prevent divide-by-zero infs. Can
+#'   be `'auto'` to use half of the minimum nonzero value. Adjusts zeros up and
+#'   ones down, but nothing else.
+#'
+#' @return
+#' @export
+#'
+#' @examples
+oddsratio <- function(x,y, add_eps = 0) {
+  if (any(x > 1 | x < 0 | y > 1 | y < 0)) {
+    rlang::abort("odds ratios need data on 0-1")
   }
+  if (add_eps == 'auto') {
+    # get the minimum
+    allvals <- c(x,y)
+    add_eps <- min(allvals[allvals > 0],
+                   na.rm = TRUE)/2
+  }
+  x[x == 0] <- add_eps
+  y[y == 0] <- add_eps
+  x[x == 1] <- 1-add_eps
+  y[y == 1] <- 1-add_eps
+  (x / (1-x)) / (y / (1-y))
+}
