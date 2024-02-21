89fd42476d771c1f3a55b6bc2f93dacee49ab40d
Galen Holt <53928785+galenholt@users.noreply.github.com>
2024-02-21 13:54:17 +1100

    patch params with group_until and pseudo-spatial

--- inst/yml/params.R
+++ inst/yml/params.R
@@ -4,13 +4,13 @@
 
 # Aggregation sequence
 aggseq <- list(ewr_code = c('ewr_code_timing', 'ewr_code'),
-               planning_unit = planning_units,
+               planning_units = planning_units,
                env_obj =  c('ewr_code', "env_obj"),
                sdl_units = sdl_units,
                Specific_goal = c('env_obj', "Specific_goal"),
                cewo_valleys = cewo_valleys,
                Objective = c('Specific_goal', 'Objective'),
-               mdb = basin,
+               basin = basin,
                target_5_year_2024 = c('Objective', 'target_5_year_2024'))
 
 # Functions for each aggregation
