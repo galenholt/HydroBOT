5f7bdc8baf4d58597a2bcc20452bbc6516a70078
Galen Holt <53928785+galenholt@users.noreply.github.com>
2025-03-06 13:25:33 +1100

    update py-ewr

--- data_creation/EWRFIX.R
+++ data_creation/EWRFIX.R
@@ -1,12 +1,13 @@
 # THis is fixing the internal EWR parameter sheet and causal networks to add SDL units and also other issues encountered.
 
-
+# This just gets the current ewr parameter sheet from the EWR tool. At the time
+# of writing, that was py-ewr 2.3.0 (Which is important, newer versions will
+# have the changes from this instead)
 ret <- get_raw_ewrsheet()
 
 thedups <- ret[duplicated(ret), ]
 
-# I could just go for this, but want to look at what I'm doing first
-ret2 <- ret[!duplicated(ret),]
+# I could just delete dups, but I want to look at it first
 
 ret |>
   dplyr::group_by(dplyr::across(tidyselect::everything())) |>
@@ -15,25 +16,34 @@ ret |>
   dplyr::arrange(dplyr::across(tidyselect::everything()))
 
 
-# readr::write_csv(ret2, 'parameter_sheet.csv', na = '', eol = '\r\n')
+# Those are true, complete dups, so go ahead and delete them.
+ret2 <- ret[!duplicated(ret),]
+# save the clean version out
+readr::write_csv(ret2, 'parameter_sheet.csv', na = '', eol = '\r\n')
 
 
 # now fix the causals
+# This just gets the current causal networks from the EWR tool. At the time
+# of writing, that was py-ewr 2.3.0 (Which is important, newer versions will
+# have the changes from this instead)
 ce <- get_causal_ewr()
 
-# wipe out the duplicates
+# wipe out complete duplicates
 ced <- purrr::map(ce, \(x) x[!duplicated(x), ])
 
 # clean up the bits we want to join on to the ewr2Obj
 retc <- cleanewrs(ret2) |>
   dplyr::select(planning_unit_name, gauge, ewr_code, ewr_code_timing, state, SWSDLName)
-# There is duplication in this, due to differences in some of the rquirements. BUt we can delete it here because we just want unique linking to SWSDLNam
+# There is more duplication in this, due to differences in some of the
+# requirements. BUt we can delete it here because we just want unique linking to
+# SWSDLName
 retc <- retc[!duplicated(retc),]
 
 # I'm ending up with a lot of NA when I do this join.
 e2o_test <- ced$ewr2obj |> dplyr::left_join(retc, relationship = 'many-to-one') # each row in the network should match to one swooslename
 sum(is.na(e2o_test$SWSDLName))
 
+# Check the NAs and figure out why
 # A few are because the network has ewr_codes that aren't in the ewr table
 eco <- unique(e2o_test$ewr_code)
 eto <- unique(retc$ewr_code)
@@ -43,7 +53,8 @@ eto[!eto %in% eco]
 
 sum(e2o_test$ewr_code %in% eco[!eco %in% eto])
 
-# We can look into that further, but it's also a different problem
+# We can look into that further, but it's also a different problem than adding
+# SWSDLNames
 
 # TO move forwardc here, are PU and SWSDLs uniquely paired?
 
@@ -57,7 +68,10 @@ pusdl |>
   dplyr::ungroup() |>
   dplyr::arrange(planning_unit_name)
 
-# so, I encountered this below, and that brought me up here. What I need to know now is whether these three catchments have the same links. ie does the same ewr --> env_obj --> target hold?
+# so, I encountered an issue with the Warrego Paroo Bulloo and Nebine below, and
+# that brought me back up here. What I need to know now is whether these three
+# catchments have the same links. ie does the same ewr --> env_obj --> target
+# hold?
 
 # When I have a gauge, it should become unique?
 pusdlg <- cleanewrs(ret2) |>
@@ -74,20 +88,24 @@ pusdlg |>
 
 
 
-# There's an issue with naming the PUs
+# There's an issue with naming the PUs.
+# I fixed the Coorong in the parameter sheet, it was a typo. but I think I'll
+# fix the Border rivers in the causal to match the parameter sheet
 ced[1:2] <- ced[1:2] |> purrr::map(\(x) {
   x$planning_unit_name[x$planning_unit_name == 'Border rivers'] <- 'Border Rivers and Moonie Long-term watering plan'
   return(x)
   })
 
-# and an issue with the gauge in the CLLMM
+# and an issue with the gauge in the CLLMM (extra 0)
 ced$ewr2obj$gauge[ced$ewr2obj$gauge == 'A42610002'] <- 'A4261002'
 
 # Let's use that then when I have a gauge
 e2o <- ced$ewr2obj |>
-  dplyr::left_join(pusdlg, relationship = 'many-to-one') # each row in the network should match to one swooslename
+  dplyr::left_join(pusdlg, relationship = 'many-to-one') # each row in the network should match to one SWSDLName
 
-# Fixing names gets us down to about 200 that don't match (though there are still a lot of pus in the table that arent' in the network). That might be OK now that we skip the PU step
+# Fixing names gets us down to about 200 that don't match (though there are
+# still a lot of pus in the table that arent' in the network). That might be OK
+# now that we skip the PU step
 sum(is.na(e2o$SWSDLName))
 
 # the PU
@@ -98,8 +116,6 @@ eco[!eco %in% eto]
 eto[!eto %in% eco]
 
 sum(e2o$planning_unit_name %in% eco[!eco %in% eto])
-
-# I fixed the Coorong in the parameter sheet, it was a typo. but I think I'll fix the Border rivers in the causal
 
 # the gauges
 eco <- unique(ced$ewr2obj$gauge)
@@ -115,7 +131,7 @@ any(duplicated(e2o))
 # Now we get to the objetive to target issue, where there are three sdls. we
 # have to assume that the objective to target mapping works the same in all 3.
 # That's likely reasonable, the detailed differences would come in at the
-# ewr-objective most likely, and that is guage-linked.
+# ewr-objective most likely, and that is gauge-linked.
 # SO, that means the network will just expand by 3 into those three sdls.
 
 pusdl <- pusdl |> dplyr::select(-l_t_w_p_short_name)
@@ -207,7 +223,7 @@ retc |> dplyr::filter(planning_unit_name == 'Warrego Paroo Bulloo and Nebine Lon
   dplyr::select(-gauge) |> dplyr::distinct() |>
   dplyr::arrange(planning_unit_name, ewr_code, ewr_code_timing) |> View()
 
-# The question reall is whether all the ewr_codes map to the same obj in each of those SWSDLs? this is a q for e2o_test
+# The question really is whether all the ewr_codes map to the same obj in each of those SWSDLs? this is a q for e2o_test
 e2o_test |> dplyr::filter(planning_unit_name == 'Warrego Paroo Bulloo and Nebine Long-term watering plan') |>
   dplyr::arrange(planning_unit_name, ewr_code, ewr_code_timing, env_obj) |> View()
 
