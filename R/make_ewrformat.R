87907e9154a59afeed7b9b9a106ac22c4bfcadc1
Galen Holt <53928785+galenholt@users.noreply.github.com>
2025-08-06 15:17:12 +1000

    helper for gauge style

---
+++ R/make_ewrformat.R
@@ -0,0 +1,23 @@
+#' Convert pulled gauge data into the EWR 'Standard time-series' format
+#'
+#' This adds the _flow and _level suffixes and cleans things up
+#' Expects data from either states or BOM, only tested using [galenholt/hydrogauge]
+#' If gauges are different lengths or need to be otherwise joined, that should be done afterwards
+#'
+#' @param gaugedata a dataframe of gauge data pulled from states or BOM
+#'
+#' @returns gauge data in 'Standard time-series' format
+#' @export
+#'
+make_ewrformat <- function(gaugedata) {
+  gaugedata <- gaugedata |>
+    dplyr::mutate(gauge = dplyr::case_when(
+      grepl('ml/d', units, ignore.case = TRUE) ~ paste0(gauge, '_flow'),
+      units %in% c('m', 'Res. Level AHD', 'Metres') ~ paste0(gauge, '_level'),
+      .default = paste0(gauge, '_FAIL'))
+    ) |>
+    dplyr::select(Date = time, gauge, value) |>
+    tidyr::pivot_wider(id_cols = Date, names_from = gauge, values_from = value) |>
+    dplyr::mutate(Date = as.Date(Date))
+  return(gaugedata)
+}
