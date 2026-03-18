daacfd72e01a3b767b1ad00a88f5159c9171b485
Galen Holt <53928785+galenholt@users.noreply.github.com>
2026-03-18 15:01:25 +1100

    networks

--- tests/testthat/test-make_nodes.R
+++ tests/testthat/test-make_nodes.R
@@ -1,12 +1,15 @@
 test_that("nodeorder", {
-  testset <- causal_ewr$ewr2obj |>
+  testset <- causal_ewr$theme_to_ewr_code_main |>
     dplyr::filter(grepl('Macq', LTWPShortName)) |>
     # dplyr::select(-ewr_code_timing, -state, -LTWPShortName, -env_obj) |>
     dplyr::distinct()
 
   testedges <- make_edges(dflist = list(testset),
-                          fromtos = list(Target = c('ewr_code', 'Target')),
+                          fromtos = list(theme = c('ewr_code_main', 'theme')),
                           gaugefilter = '421019')
 
   testnodes <- make_nodes(testedges)
+
+  expect_equal(names(testnodes), c('Name', 'NodeType', 'nodeorder'))
+  expect_s3_class(testnodes, 'tbl_df')
 })
--- tests/testthat/test-plot_prep.R
+++ tests/testthat/test-plot_prep.R
@@ -9,11 +9,11 @@ test_that("character sceneorder works", {
   test_prep <- plot_data_prep(agg_theme_space$sdl_units, outcome_col = 'ewr_achieved',
                          sceneorder = c('down4', 'base', 'up4'))
 
-  expect_equal(levels(test_prep$data$scenario), c('down4', 'base', 'up4', 'MAX'))
+  expect_equal(levels(test_prep$data$scenario), c('down4', 'base', 'up4'))
 })
 
 test_that("factor sceneorder works", {
-  scenarios <- tibble::tibble(scenario = c('down4', 'base', 'up4', 'MAX'), delta = c(1, 0.25, 4, Inf))
+  scenarios <- tibble::tibble(scenario = c('down4', 'base', 'up4'), delta = c(1, 0.25, 4))
   sceneorder_f <- forcats::fct_reorder(scenarios$scenario, scenarios$delta)
 
   test_prep <- plot_data_prep(agg_theme_space$sdl_units, outcome_col = 'ewr_achieved',
